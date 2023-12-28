5f03c6a063216b7f8a8040ca0e61543465a81771
mamunur rahman <rahmanmamun291@gmail.com>
2023-12-28 14:20:09 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      // colors: {
+      //   clifford: '#da373d',
+        
+        'bike': "#26c426"
+      // },
+    },
+    // colors: {
+    //   primary: "#da373d",
+    // }
+  },
+  plugins: [],
+  safelist: ['bg-bike'],
+  // safelist: [
+  //   {
+  //     pattern:
+  //       /(bg|text|border)-(primary | blue)/
+  //   },
+  // ],
+};
